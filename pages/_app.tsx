fd1132db25d97bae383ba44f6299828886726b4c
Sasindu Kavinda <kaviliya123@gmail.com>
2023-07-05 17:06:25 -0400

    commit2

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import MainLayout from "../layouts/main";
+import "../public/reset.css";
+import "../styles/globals.css";
+import withTheme from "../theme";
+import { Analytics } from "@vercel/analytics/react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return withTheme(
+    MainLayout(
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+    )
+  );
+}
